0fa25e0b0c89f6dc89d93846271c43b925f0f298
breck100 <75516893+breck100@users.noreply.github.com>
2023-02-18 09:34:47 +0200

    Adding global variables to utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 # a method to check if a number is in range
 import random
 
+global SCORES_FILE_NAME, BAD_RETURN_CODE
+SCORES_FILE_NAME = "scores.txt"
+BAD_RETURN_CODE = 499
+
 def is_num_in_range(num ,start ,end):
    if num.isnumeric() and int(num) in range(start, end+1):
           return True
