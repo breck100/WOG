3d9f43363c508dab38700cf720deedd3e352201d
Cohen, Eran <75516893+breck100@users.noreply.github.com>
2023-06-12 21:41:53 +0300

    adding comments - prepare for sending WOG4

--- Scores/MainScores.py
+++ Scores/MainScores.py
@@ -1,18 +1,13 @@
 import utils
 from flask import Flask, request, render_template
-import os
 
 app = Flask(__name__)
 @app.route("/")
 def score_server():
-
-    print ("3")
-    # f = open(utils.SCORES_FILE_NAME, 'r')
     f = open(utils.SCORES_FILE_FOR_FLASK, 'r')
     score = f.readline()
     f.close()
     print("score=" + score)
-    # return("success", 201)
     f1 = 1
     try:
         f1 = render_template('score.html', score=score)
